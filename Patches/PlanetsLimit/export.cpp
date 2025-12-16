d270fb0d6196bb9707491d567117c10a1264855c
Lane Dibello <lane.dibello@gmail.com>
2025-12-15 21:32:26 -0600

    init functions

--- Patches/PlanetsLimit/export.cpp
+++ Patches/PlanetsLimit/export.cpp
@@ -1,8 +1,8 @@
 #include "Common.h"
 
-extern "C" void __cdecl ClearPlanets(void* partyTableThis) {
-    int* availablePlanets = getObjectProperty<int*>(partyTableThis, 0x60);
-    int* selectablePlanets = getObjectProperty<int*>(partyTableThis, 0xa0);
+extern "C" void __cdecl ClearPlanets(void* partyTable) {
+    int* availablePlanets = getObjectProperty<int*>(partyTable, 0x60);
+    int* selectablePlanets = getObjectProperty<int*>(partyTable, 0xa0);
 
     memset(availablePlanets, 0, 4 * 0x7f);
     memset(selectablePlanets, 0, 4 * 0x7f);
@@ -22,13 +22,21 @@ extern "C" void __cdecl WritePlanetMask(void* gff, void* strct, void* partyTable
 extern "C" void __cdecl ReadPlanetMask(void* gff, void* strct, void* partyTable) {
     //TODO: write this out:
     /*
-    * Check if AvailablePlanets/SelectablePlanets exists
+    * Check if AvailablePlanets/SelectablePlanets exists (see: CResGFF::GetFieldByLabel)
     * If not, use GlxyMapPlntMsk and return
     * Otherwise:
     * CResGFF::GetList/GetListCount -> "AvailablePlanets"
     * CResGFF::GetList/GetListCount -> "SelectablePlanets"
     * Loop through each list and populate PartyTable planets
     */
+}
+
+extern "C" void __cdecl InitializePartyTablePlanets(void* partyTable) {
+    int* availablePlanets = new int[0x7f];
+    int* selectablePlanets = new int[0x7f];
+
+    setObjectProperty<int*>(partyTable, 0x60, availablePlanets);
+    setObjectProperty<int*>(partyTable, 0xa0, selectablePlanets);
 }
 
 // DLL Entry Point
