f5fb31f445979a32545ace4b5779bdb9c802513d
Lane Dibello <lane.dibello@gmail.com>
2025-12-25 16:49:16 -0600

    check point

---
+++ Patches/Common/GameAPI/CResGFF.h
@@ -0,0 +1,14 @@
+/*
+
+Offsets:
+===
+id, class_name, member_name, offset, notes
+34, CResGFF, header, 64, GFFHeaderInfo *
+35, CResGFF, structs, 68, GFFStructData *
+36, CResGFF, fields, 76, GFFFieldData *
+37, CResGFF, labels, 84, char[16] *
+38, CResGFF, field_type, 141, char[4]
+===
+
+Functions
+*/
