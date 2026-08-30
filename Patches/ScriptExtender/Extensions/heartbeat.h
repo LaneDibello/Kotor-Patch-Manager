947efdfbb414cb1f6ebde1c102307b41e02155a0
Lane Dibello <lane.dibello@gmail.com>
2026-08-30 13:11:58 -0500

    checkpoint

---
+++ Patches/ScriptExtender/Extensions/heartbeat.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "Common.h"
+#include "GameAPI/CVirtualMachine.h"
+
+const int ForceHeartbeatIndex = 803;
+VirtualMachineReturnTypes __stdcall ExecuteForceHeartbeat(DWORD routine, int paramCount);
+
+/*
+Something like this for the timestamp logic:
+where (D, M) is the result of CWorldTimer::GetWorldTime
+```
+offset = 7000                       // > 6000, gives headroom
+if (M > offset):
+    field43_0x348 = D
+    field44_0x34c = M - offset      // non-zero, in the past
+else:                               // borrow a day
+    field43_0x348 = D - 1
+    field44_0x34c = M - offset + day_length_milliseconds   // large, non-zero
+```
+*/
