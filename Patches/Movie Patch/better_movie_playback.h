2095b230db2799a488d527caa8dbe6b316b156ec
J <41028351+J0-o@users.noreply.github.com>
2026-09-11 19:48:12 +0000

    MoviePatchV1.5.1
    - added Bicubic filter to movie playback
    - Kotor2 support added for the filter

--- Patches/Movie Patch/better_movie_playback.h
+++ Patches/Movie Patch/better_movie_playback.h
@@ -6,6 +6,45 @@ namespace BetterMoviePlayback {
 
 constexpr int BaseWidth = 800;
 constexpr int BaseHeight = 600;
+constexpr int MaximumMovieDimension = 16384;
+constexpr int MaximumTextureUnitsToReset = 32;
+
+constexpr SIZE_T MovieBinkOffset = 0x48;
+constexpr SIZE_T MovieBufferOffset = 0x4C;
+constexpr SIZE_T MovieWindowOffset = 0x50;
+constexpr SIZE_T MovieOffsetXOffset = 0x84;
+constexpr SIZE_T MovieOffsetYOffset = 0x88;
+constexpr DWORD BinkFrameBufferOffset = 0x34;
+
+constexpr DWORD BinkBufferBlitIatAddress = 0x0073D460;
+constexpr DWORD BinkCopyToBufferIatAddress = 0x0073D46C;
+constexpr DWORD BinkBufferSetOffsetIatAddress = 0x0073D480;
+constexpr DWORD BinkBufferSetScaleIatAddress = 0x0073D484;
+constexpr DWORD ScreenWidthAddress = 0x0078D1D4;
+constexpr DWORD ScreenHeightAddress = 0x0078D1D8;
+
+struct MovieFilterAddresses {
+    DWORD binkBufferBlitIatAddress;
+    DWORD binkCopyToBufferIatAddress;
+    DWORD screenWidthAddress;
+    DWORD screenHeightAddress;
+};
+
+constexpr MovieFilterAddresses Kotor1MovieFilterAddresses = {
+    BinkBufferBlitIatAddress,
+    BinkCopyToBufferIatAddress,
+    ScreenWidthAddress,
+    ScreenHeightAddress
+};
+
+constexpr MovieFilterAddresses Kotor2GogMovieFilterAddresses = {
+    0x009854BC,
+    0x009854C8,
+    0x009F22A4,
+    0x009F22A8
+};
+
+constexpr SIZE_T Kotor2MoviePointerFrameOffset = 0xE0;
 
 extern volatile int* const ScreenWidth;
 extern volatile int* const ScreenHeight;
@@ -13,5 +52,15 @@ extern volatile int* const ScreenHeight;
 int screenWidth();
 int screenHeight();
 bool safeReadDword(const void* address, DWORD& value);
+bool prepareKotor2MovieContext(void* movie);
+void releaseKotor2MovieContext();
+bool blitMovieMitchellNetravaliImpl(
+    void* movie,
+    unsigned int rectCount,
+    const MovieFilterAddresses& addresses);
+void blitMovieOriginalImpl(
+    void* movie,
+    unsigned int rectCount,
+    const MovieFilterAddresses& addresses);
 
 }
