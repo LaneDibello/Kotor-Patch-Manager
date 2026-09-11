4a38e5177e074426a7785803a5810ee1391194b7
Lane Dibello <lane.dibello@gmail.com>
2026-09-11 10:44:21 -0500

    graphics options

--- Patches/ExpandedVanillaOptions/handlers.cpp
+++ Patches/ExpandedVanillaOptions/handlers.cpp
@@ -20,41 +20,41 @@ extern "C" void __cdecl AdditionalOptions_GraphicsHandler(const char* key, const
         CClientOptions clientOptions;
         clientOptions.SetFullScreenEnabled(atoi(value));
     }
-    else if (!strcmp(key, "Disable Vertex Buffer Objects") {
+    else if (!strcmp(key, "Disable Vertex Buffer Objects")) {
         BYTE* disabled = static_cast<BYTE*>(GameVersion::GetGlobalPointer("disableVertexBufferObjects"));
         *disabled = static_cast<BYTE>(atoi(value));
-
-        //probably need to re-init aurora here
+    }
+    else if (!strcmp(key, "Disable Write-Only VBO")) {
+        int* enabled = static_cast<int*>(GameVersion::GetGlobalPointer("vertexBufferObjectsWriteOnly"));
+        *enabled = static_cast<int>(atoi(value) == 0);
     }
     else if (!strcmp(key, "AllowWindowedMode")) {
-
+        int* allowed = static_cast<int*>(GameVersion::GetGlobalPointer("allowWindowedMode"));
+        *allowed = atoi(value);
     }
     else if (!strcmp(key, "DisableGUI")) {
-
+        int* disabled = static_cast<int*>(GameVersion::GetGlobalPointer("noGUIRender"));
+        *disabled = atoi(value);
     }
     else if (!strcmp(key, "RenderLevel")) {
-
+        int* level = static_cast<int*>(GameVersion::GetGlobalPointer("renderLevel"));
+        *level = atoi(value);
     }
     else if (!strcmp(key, "Do3dGui")) {
-
+        int* enabled = static_cast<int*>(GameVersion::GetGlobalPointer("do3DGUI"));
+        *enabled = atoi(value);
     }
     else if (!strcmp(key, "VisibilityGraph")) {
-
-    }
-    else if (!strcmp(key, "DisableSaturation")) {
-
-    }
-    else if (!strcmp(key, "ScanNoise")) {
-
-    }
-    else if (!strcmp(key, "FilmNoise")) {
-
+        int* enabled = static_cast<int*>(GameVersion::GetGlobalPointer("enableVisibilityGraph"));
+        *enabled = atoi(value);
     }
     else if (!strcmp(key, "Beams")) {
-
+        int* enabled = static_cast<int*>(GameVersion::GetGlobalPointer("enableBeams"));
+        *enabled = atoi(value);
     }
     else if (!strcmp(key, "DoGrassWind")) {
-
+        int* enabled = static_cast<int*>(GameVersion::GetGlobalPointer("enableGrassWind"));
+        *enabled = atoi(value);
     }
     else {
         debugLog("[ExpandedVanillaOptions] Unknown graphics key %s, with length %u", key, strlen(key));
@@ -158,6 +158,30 @@ extern "C" void __cdecl ReadVideoModeSettings_Hook(void* iniPtr) {
     CExoString emitters("Emitters");
     if (ini.ReadIniEntry(&value, &SwkotorIni(), &GraphicsCat(), &emitters)) {
         AdditionalOptions_GraphicsHandler(emitters.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString disableGUI("DisableGUI");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &disableGUI)) {
+        AdditionalOptions_GraphicsHandler(disableGUI.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString renderLevel("RenderLevel");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &renderLevel)) {
+        AdditionalOptions_GraphicsHandler(renderLevel.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString do3dGui("Do3dGui");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &do3dGui)) {
+        AdditionalOptions_GraphicsHandler(do3dGui.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString visibilityGraph("VisibilityGraph");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &visibilityGraph)) {
+        AdditionalOptions_GraphicsHandler(visibilityGraph.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString beams("Beams");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &beams)) {
+        AdditionalOptions_GraphicsHandler(beams.GetCStrSafe(), value.GetCStrSafe());
+    }
+    CExoString doGrassWind("DoGrassWind");
+    if (ini.ReadIniEntry(&value, &AddlIni(), &GraphicsCat(), &doGrassWind)) {
+        AdditionalOptions_GraphicsHandler(doGrassWind.GetCStrSafe(), value.GetCStrSafe());
     }
 
 }
